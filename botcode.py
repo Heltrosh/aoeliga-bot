5a2c89402f5d850cf1c8fbd5692b6695db5acd25
Heltrosh <haskyhassan777@gmail.com>
2021-08-02 19:35:37 +0200

    dbtest

--- botcode.py
+++ botcode.py
@@ -4,21 +4,28 @@ import os
 import challonge
 import psycopg2
 
-conn = psycopg2.connect(os.getenv("DATABASE_URL"), sslmode='require')
+conn = psycopg2.connect(os.getenv("DATABASE_URL"), sslmode='disable')
+cursor = conn.cursor()
+cursor.execute("SELECT ID,CHALLONGE,DISCORD FROM USERS")
+rows = cursor.fetchall()
+for row in rows:
+  print(row)
+cursor.close()
+conn.close()
 
-challonge.set_credentials("Heltrosh", os.getenv("CHALLONGE_KEY"))
+#challonge.set_credentials("Heltrosh", os.getenv("CHALLONGE_KEY"))
 
-tournament = challonge.tournaments.show(10110170)
-for match in challonge.matches.index(tournament["id"]):
-  if match["round"] == 1 and match["state"] == "open":
-    retard = challonge.participants.show(tournament["id"], match["player1_id"])
-    retard2 = challonge.participants.show(tournament["id"], match["player2_id"])
-    print(retard["name"] + " " + retard2["name"])
+#tournament = challonge.tournaments.show(10110170)
+#for match in challonge.matches.index(tournament["id"]):
+#  if match["round"] == 1 and match["state"] == "open":
+#   retard = challonge.participants.show(tournament["id"], match["player1_id"])
+#    retard2 = challonge.participants.show(tournament["id"], match["player2_id"])
+#    print(retard["name"] + " " + retard2["name"])
 
-client = commands.Bot(command_prefix = '!')
+#client = commands.Bot(command_prefix = '!')
 
-@client.event
-async def on_ready():
-    print('Bot is ready.')
+#@client.event
+#async def on_ready():
+#    print('Bot is ready.')
 
-client.run(os.getenv("DISCORD_TOKEN"))
+#client.run(os.getenv("DISCORD_TOKEN"))
