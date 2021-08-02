0df232a6d6cef36258942356208a43ea75dc8a5c
Heltrosh <88330405+Heltrosh@users.noreply.github.com>
2021-08-02 16:08:28 +0200

    Update botcode.py

--- botcode.py
+++ botcode.py
@@ -1,1 +1,10 @@
+import discord
+from discord.ext import commands
 
+client = commands.Bot(command_prefix = '!')
+
+@client.event
+async def on_ready():
+    print('Bot is ready.')
+
+client.run('ODcxNDU3ODMwMjExODQyMTA4.YQbmWQ.NlYHeaBTQ7j4ewMYY_zkYSFG8fg')
