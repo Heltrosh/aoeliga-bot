d674ad5d4d02e576ce0e88384c3ac23e86c954c5
Heltrosh <haskyhassan777@gmail.com>
2021-08-02 17:10:11 +0200

    ChallongeTest

--- botcode.py
+++ botcode.py
@@ -1,6 +1,16 @@
 import discord
 import os
+import challonge
 from discord.ext import commands
+
+challonge.set_credentials("Heltrosh", os.getenv("CHALLONGE_KEY"))
+
+tournament = challonge.tournaments.show(10110170)
+for match in challonge.matches.index(tournament["id"]):
+  if match["round"] == 1 and match["state"] == "open":
+    retard = challonge.participants.show(tournament["id"], match["player1_id"])
+    retard2 = challonge.participants.show(tournament["id"], match["player1_id"])
+    print(retard["name"] + " " + retard2["name"])
 
 client = commands.Bot(command_prefix = '!')
 
