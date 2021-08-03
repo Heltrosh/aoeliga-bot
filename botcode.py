d9ca645fada393ea8867be0038751a1283eb7cb9
Heltrosh <haskyhassan777@gmail.com>
2021-08-03 10:30:11 +0200

    Working version before knowing details

--- botcode.py
+++ botcode.py
@@ -1,41 +1,56 @@
 import discord
-from discord.client import Client
 from discord.ext import commands
+from dotenv.main import load_dotenv
 import os
 import challonge
 import psycopg2
 
-def getDiscordName(challongename):
+
+def getDiscordID(challongename):
   i=0
-  discordName=""
+  discordID=0
   conn = psycopg2.connect(os.getenv("DATABASE_URL"), sslmode='require')
   cursor = conn.cursor()
-  cursor.execute("SELECT DISCORD FROM USERS WHERE CHALLONGE= %s",[challongename])
-  rows = cursor.fetchall()
-  for row in rows:
-    discordName = row[0]
+  cursor.execute("SELECT DISCORDID FROM USERSTEST WHERE CHALLONGE= %s",[challongename])
+  row = cursor.fetchone()
+  discordID = row[0]
   cursor.close()
   conn.close()
-  return discordName
+  return discordID
 
 def getDelayers(round):
+  lazies = []
   challonge.set_credentials("Heltrosh", os.getenv("CHALLONGE_KEY"))
   tournament = challonge.tournaments.show(10110170)
   for match in challonge.matches.index(tournament["id"]):
     if match["round"] == int(round) and match["state"] == "open":
-      retard = challonge.participants.show(tournament["id"], match["player2_id"])
-      return retard["name"]
+      lazies.append(challonge.participants.show(tournament["id"], match["player1_id"])["name"])
+      lazies.append(challonge.participants.show(tournament["id"], match["player2_id"])["name"])
+  return lazies
 
-client = commands.Bot(command_prefix = '!')
-@client.event
-async def on_ready():
-  print('We have logged in as {0.user}'.format(client))
+def main():
+  load_dotenv()
+  bot = commands.Bot(command_prefix = '!')
+  
+  @bot.event
+  async def on_ready():
+    print('I have logged in as {0.user}'.format(bot))
 
-@client.command()
-async def delayerlist(ctx, round):
-  delayerName = getDelayers(round)
-  discordName = getDiscordName(delayerName)
-  await ctx.send(discordName)
+  @bot.command()
+  async def pinground(ctx, round):
+    i=0
+    lazies = getDelayers(round)
+    for lazy in lazies:
+      discordID = getDiscordID(lazy)
+      user = await bot.fetch_user(discordID)
+      await user.send('Lenochu')
+      i+=1
+    await ctx.send('Poslal jsem ' + str(i) + ' zpráv.')
+  bot.run(os.getenv("DISCORD_TOKEN"))
 
+if __name__ == "__main__":
+    main()
 
-client.run(os.getenv("DISCORD_TOKEN"))
+# IMPORTANT TO DO:  Only administrator can issue commands
+#                   Prepare for multiple tournaments
+#                   Think deadlines through (different league systems?)
