136839e2b25bcdaed0f0dfb6f290e34ccada54bd
Heltrosh <haskyhassan777@gmail.com>
2022-06-29 07:22:44 +0200

    added kaper to msg resending

--- botcode.py
+++ botcode.py
@@ -157,6 +157,8 @@ def main():
       user = await bot.fetch_user(164698420777320448)
       resendMessage = 'From: ' + message.author.name + '\n' + 'Content: ' + message.content
       await user.send(resendMessage)
+      user = await bot.fetch_user(687276408057233658)
+      await user.send(resendMessage)
     await bot.process_commands(message)
 
 #COMMANDS
